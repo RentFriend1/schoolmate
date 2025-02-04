a7e0fce12aa1a2943b96391236e0c99381fd5bbf
Michal Bernát <michal.bernat.l312@gmail.com>
2025-02-04 09:15:21 +0100

    login / logout buttons fix

--- schoolmate/src/signin/signin.component.ts
+++ schoolmate/src/signin/signin.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
+  imports: [CommonModule], // Add CommonModule to imports
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
@@ -12,9 +15,11 @@ export class SigninComponent {
     public authService: AuthService
   ) { }
 
-
   signInPopup() {
     this.authService.GoogleAuthPopup();
   }
 
+  signOut() {
+    this.authService.SignOut();
+  }
 }
